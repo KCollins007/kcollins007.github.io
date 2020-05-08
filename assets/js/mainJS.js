1f13c3d2efce9ee5a8fb221c3683aebc5859d0d1
kristen collins <collikj1@student.op.ac.nz>
2020-05-08 16:51:33 +1200

    Project 1 submitted

--- assets/js/mainJS.js
+++ assets/js/mainJS.js
@@ -1,45 +1,67 @@
+let image = document.querySelectorAll(".imageSection");
+let artist = document.querySelectorAll(".artistSection");
+let song = document.querySelectorAll(".songSection");
+let lastlisten = document.querySelectorAll(".lastlistenSection");
+let image2 = document.querySelectorAll(".image2Section")
+let image3 = document.querySelectorAll(".image3Section");
+let artist3 = document.querySelectorAll(".artist3Section");
+let totallisten3 = document.querySelectorAll(".totallisten3Section");
+let song3 = document.querySelectorAll(".song3Section");
 
-    //Variables
-let listenedDiv =document.querySelectorAll("#listened div");
-console.log(listenedDiv);
-
-//Fetching music information
-fetch("assets/JS/music.json")
+fetch("assets/js/music.json")
 .then(res => res.json())
 .then(data => {
-    data.forEach(function(object) {
-        object.lastlisten.forEach(function(item, index){
-            console.log(item);
-            listenedDiv[index].style.background = `url(${item["image"]}) no-repeat center center/cover`;
-            listenedDiv[index].style.width = "200px";
-            listenedDiv[index].style.height = "200px";
+    //data is a javaScript object in this case
 
-            listenedDiv[index].innerHTML = `${item["artist"]} ${item["song"]} ${item["last listen"]}`;
+    const {lastlistened, topartists, toptracks} = data //pull out keys from an object and use them as variables
+
+    lastlistened.forEach(function(item, index) {   
+            console.log(item);
+            //image
+            image[index].style.background = `url(${item["image"]}) no-repeat center center/cover`;
+            image[index].style.width = "100px";
+            image[index].style.height = "100px";
+            //data
+            song[index].innerHTML = `\"${item["song"]}\" `;
+            artist[index].innerHTML = `${item["artist"]}`;
+            lastlisten[index].innerHTML = `last listened <br><br>${item["last listen"]}`;          
         });
-   });
+
+        topartists.forEach(function(item, index) {   
+            console.log(item);
+            image2[index].style.background = `url(${item["image"]}) no-repeat center center/cover`;
+            image2[index].style.width = "200px";
+            image2[index].style.height = "200px";
+
+            image2[index].innerHTML = `<br><br><br><br><br>&nbsp;&nbsp;&nbsp;${item["artist"]}
+            <br><br>&nbsp;&nbsp;&nbsp;${item["total listens"]}<br>`;
+        });
+
+        toptracks.forEach(function(item, index){
+            console.log(item);
+            image3[index].style.background = `url(${item["image"]}) no-repeat center center/cover`;
+            image3[index].style.width = "80px";
+            image3[index].style.height = "80px";
+
+            song3[index].innerHTML = `${item["song"]}<br><br>${item["artist"]}`;
+            totallisten3[index].innerHTML = `${item["total listens"]}`;
+        });
 });
 
-let artistDiv = document.querySelectorAll("#artist div")
-console.log(artistDiv);
-
-fetch("assets/JS/music.json")
-.then(res => res.json())
-.then(data => {
-    data.forEach(function(object) {
-        object.topartists.forEach(function(item, index){
-            console.log(item);
-            artistDiv[index].style.background = `url(${item["image"]}) no-repeat center center/cover`;
-            artistDiv[index].style.width = "200px";
-            artistDiv[index].style.height = "200px";
-
-            artistDiv[index].innerHTML = `${item["artist"]} ${item["total listens"]}`;
-        });
-   });
-});
-
-
-function results()
+function results() //In Contacts page where user inputs contact data
 {
     let name = document.getElementById('name').value;
-    document.write("Thank you for your message "+ name);
+    alert (`Thank you for your name, phone number, and email address, ${name}`);
 }
+
+function myFunction(x) { //like/unlike heart function
+    if ( x.classList.contains( "fa-heart") ) {
+        x.classList.remove( "fa-heart" );
+        x.classList.add( "fa-heart-o" );
+    }
+    else {
+        x.classList.remove( "fa-heart-o" );
+        x.classList.add( "fa-heart" );
+    }
+}
+
